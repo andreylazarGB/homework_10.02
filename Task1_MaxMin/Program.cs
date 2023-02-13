ba1bea4a14dd013f2ba69ed3805653437859b728
andrey lazar <andreylazarmr@gmail.com>
2023-02-13 21:20:13 +0300

    Task1 MaxMin

---
+++ Task1_MaxMin/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());;
+Console.WriteLine("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());;
+
+if ( b > a) 
+{
+   Console.WriteLine("min = " + a); 
+   Console.WriteLine("max = " + b);
+}
+else
+    {
+     Console.WriteLine("min = " + b);
+     Console.WriteLine("max = " + a);
+    }
