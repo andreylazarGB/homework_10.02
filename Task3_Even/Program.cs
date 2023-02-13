28968d2db995a17575ba6b040b99461dede14f4e
andrey lazar <andreylazarmr@gmail.com>
2023-02-13 22:42:50 +0300

    Task3 Even

--- Task3_Even/Program.cs
+++ Task3_Even/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());;
+
+if (a % 2 == 0) 
+{
+    Console.WriteLine("число является четным "); 
+}
+else
+{
+    Console.WriteLine("число не является четным "); 
+}
+
